7b8756c3f35fdf9805fed6981bafc4e223120b7f
Luis del Moral Martínez <ldmoral1987>
2020-10-28 18:04:10 +0100

    cambio en la documentación del patrón factory method

--- src/com/ceslopedevega/patrones/creacionales/dependencyinjection/inventory/InventoryLocation.java
+++ src/com/ceslopedevega/patrones/creacionales/dependencyinjection/inventory/InventoryLocation.java
@@ -1,7 +1,9 @@
 package com.ceslopedevega.patrones.creacionales.dependencyinjection.inventory;
 
 /**
-* Esta clase representa al servicio de inventario de la aplicación. 
+* Esta clase representa al servicio de inventario de la aplicación. La clase admite
+* como ubicación de localización a cualquier objeto que implemente la interfaz
+* Inventory
 *
 * @author Luis del Moral Martínez
 */
@@ -15,11 +17,17 @@ public class InventoryLocation {
 		this.location = location;
 	}
 	
+	/**
+	* Devuelve el nombre de la ubicación del inventario
+	*/
 	public String getName ()
 	{
 		return this.name;
 	}
 	
+	/**
+	* Devuelve la capacidad de la ubicación del inventario
+	*/	
 	public int getCapacity ()
 	{
 		return this.location.calculateInventoryCapacity();
